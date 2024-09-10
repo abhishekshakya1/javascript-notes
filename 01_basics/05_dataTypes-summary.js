1bbbd8bfecb4fbd1a84598738c60b2ad17341720
abhishek321profile <124267355+abhishek321profile@users.noreply.github.com>
2024-09-10 10:52:51 +0000

    container config done

---
+++ 01_basics/05_dataTypes-summary.js
@@ -0,0 +1,43 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const name = "Abhishek"
+const score = 100
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id === anotherId);
+
+const bigNumber = 343623274895905975863n
+console.log(bigNumber);
+console.log(typeof bigNumber);
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heroes = ["Virat", "Kohli", "Ronaldo"];
+
+let student = {
+    name: "Abhishek",
+    age: 18,
+    class: "BCA"
+};
+
+const myFunction = function() {
+    console.log("Hello World");
+    
+}
+console.log(heroes, student);
+console.log(typeof (heroes), typeof (student), typeof (myFunction));
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
