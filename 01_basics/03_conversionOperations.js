081485a7f4344cbda3aec477fe5295791ba49a1d
abhishek321profile <124267355+abhishek321profile@users.noreply.github.com>
2024-09-10 06:18:02 +0000

    container config done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,39 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// "Abhishek" => NaN
+// true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+
+let isloggedIn = "Abhishek"
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "Abhishek" => true
+
+
+let someNumber = 18
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
