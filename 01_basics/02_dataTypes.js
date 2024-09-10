6697e055bce5ccd80d887b6aabcd629f77fd168c
abhishek321profile <124267355+abhishek321profile@users.noreply.github.com>
2024-09-10 05:48:56 +0000

    container config done

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -7,3 +7,23 @@ console.log(3
 )  // code readability should be high
 
 console.log("Abhishek");
+
+
+let name = "Abhishek"
+let age = 18
+let isloggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);    // object
