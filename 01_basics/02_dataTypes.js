f32a30114e081c0037265b83b62745aa37de7583
abhishek321profile <124267355+abhishek321profile@users.noreply.github.com>
2024-09-10 05:36:54 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict";  // treat all Js code as newer version
+
+// alert( 3 + 3);  // We are using node.js, not browser
+
+console.log(3
+    +3
+)  // code readability should be high
+
+console.log("Abhishek");
