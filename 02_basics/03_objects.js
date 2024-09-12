0c34414b3efbe57d6fd13d247f0ca69b9a8d6d80
abhishek321profile <124267355+abhishek321profile@users.noreply.github.com>
2024-09-12 16:30:50 +0000

    container config done

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -11,7 +11,8 @@ const JsUser = {
     name: "Abhishek",
     "full_name": "Abhishek Kumar",
     "last name": "Shakya",
-    [mySym]: "mykey1",
+    mySym: "mykey1",               // that's wrong 
+    [mySym]: "mykey1",            // it should be use like that to use as Symbol
     age: 19,
     location: "Faridabad",
     email: "abhi@google.com",
@@ -20,20 +21,38 @@ const JsUser = {
 }
 
 console.log(JsUser.email);
-// console.log(JsUser[email]);
-// console.log(JsUser["email"]);
+console.log(JsUser[email]);      // this is not allowed
+console.log(JsUser["email"]);
+
 console.log(JsUser.full_name);
 console.log(JsUser["full_name"]);
 
-// console.log(JsUser.last name);
+// console.log(JsUser.last name);   // thi is not allowed
 console.log(JsUser["last name"]);
-// console.log(JsUser.mySym);
-// console.log(typeof JsUser.mySym);
-console.log(JsUser[mySym]);
+
+console.log(JsUser.mySym);          // this is not the right way to access Symbol
+console.log(typeof JsUser.mySym);
+console.log(JsUser[mySym]);           // this is the right way to access Symbol
 console.log(typeof JsUser[mySym]);
 
 JsUser.email = "suraj@12.com"
 console.log(JsUser["email"]);
 
+Object.freeze(JsUser)
+JsUser.email = "aman@123.com"
+console.log(JsUser);
+
+
+JsUser.greeting = function() {
+    console.log("Hello JS user");
+}
+
+JsUser.greetingTwo = function() {
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser);
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
 
 
