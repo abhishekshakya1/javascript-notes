f8bc1438f134aaf102dca05907c558dde71def34
abhishek321profile <124267355+abhishek321profile@users.noreply.github.com>
2024-09-12 17:48:09 +0000

    container config done

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,32 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+console.log(courseInstructor);
+console.log(instructor);
+
+
+// const navbar = ({company}) => {
+
+// }
+
+// navbar(company = "hitesh")     // objects destructuring
+
+
+
+{
+    "name": "abhishek",
+    "coursename": "js in hindi",
+    "price": "free"
+}
+
+[
+    {},
+    {},
+    {}
+]
