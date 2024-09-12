cfc2fc618774c6da3d154684b14eb81f480bcd14
abhishek321profile <124267355+abhishek321profile@users.noreply.github.com>
2024-09-12 09:34:38 +0000

    container config done

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -4,29 +4,29 @@ const myArr = [2, 3, 4, 5, 6]
 const myHeros = ["virat", "ronaldo", "kalki"]
 
 const myArr2 = new Array(1, 2, 3, 4)
-// console.log(myArr[1]);
+console.log(myArr[1]);
 
 
 // array methods
 
-// myArr.push(7)
-// myArr.push(8)
-// myArr.push(9, 10)
-// myArr.pop()
+myArr.push(7)
+myArr.push(8)
+myArr.push(9, 10)
+myArr.pop()
 
-// myArr.unshift(0)
-// myArr.shift()
+myArr.unshift(0)
+myArr.shift()
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(19));
-// console.log(myArr.indexOf(4));
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(19));
+console.log(myArr.indexOf(4));
 
-// const newArr = myArr.join()
+const newArr = myArr.join()
 
-// console.log(myArr);
-// console.log(typeof myArr);
-// console.log(newArr);
-// console.log(typeof newArr);
+console.log(myArr);
+console.log(typeof myArr);
+console.log(newArr);
+console.log(typeof newArr);
 
 
 // slice, splice
@@ -41,6 +41,12 @@ console.log("B ", myArr);
 const myn2 = myArr.splice(1, 3)
 console.log("C ", myArr);
 console.log(myn2);
+
+/*
+   slice method does not manipulate the original array
+   But
+   splice method manipulates the original array
+*/
 
 
 
