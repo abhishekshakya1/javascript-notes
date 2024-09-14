6252f09ce594f5fce00bc2684f8ed08744962666
abhishek321profile <124267355+abhishek321profile@users.noreply.github.com>
2024-09-14 04:20:34 +0000

    container congif done

---
+++ 03_basics/05_iife.js
@@ -0,0 +1,27 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+// function chai() {
+//     console.log(`DB CONNECTED`);
+// }
+// chai()
+
+(function chai() {
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();                            
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+})("aman");
+ 
+// ( function aurcode() {
+//     console.log(`DB CONNECTED TWO`);
+// })();
+
+/*
+ wo function jo immediately execute ho jaye
+ global scope ke pollution se problem hoti hai kai baar
+ us global scope ke pollution aur jo bhi waha variables,
+  declarations hai usko hatane ke liye humne IIFE ka use kiya
+
+*/
