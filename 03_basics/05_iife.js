6f529ca25e741406945e2ad07401065ebb6af12d
abhishek321profile <124267355+abhishek321profile@users.noreply.github.com>
2024-09-14 04:27:19 +0000

    container congif done

--- 03_basics/05_iife.js
+++ 03_basics/05_iife.js
@@ -1,9 +1,9 @@
 // Immediately Invoked Function Expressions (IIFE)
 
-// function chai() {
-//     console.log(`DB CONNECTED`);
-// }
-// chai()
+function chai() {
+    console.log(`DB CONNECTED`);
+}
+chai();
 
 (function chai() {
     // named IIFE
@@ -14,9 +14,9 @@
     console.log(`DB CONNECTED TWO ${name}`);
 })("aman");
  
-// ( function aurcode() {
-//     console.log(`DB CONNECTED TWO`);
-// })();
+( function aurcode() {
+    console.log(`DB CONNECTED TWO`);
+})();
 
 /*
  wo function jo immediately execute ho jaye
