7bbf6c887c0a8f0fe8419aecd7aa325f374a9f1b
abhishek321profile <124267355+abhishek321profile@users.noreply.github.com>
2024-09-15 12:19:12 +0000

    container config done

---
+++ 05_iterations/do.js
@@ -0,0 +1,33 @@
+// while loop
+
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of index is: ${index}`);
+    index = index+2
+}
+
+let myArr = ["flash", "batman", "superman", "ironman"]
+let i = 0
+
+while (i < myArr.length) {
+    // console.log(`The super hero is: ${myArr[i]}`);
+    // index = index++
+    // index = ++index
+    // index++
+    // ++index
+    // index + 1
+    i = i + 1
+}
+
+
+
+// do while loop
+
+let score = 1
+// let score = 11
+
+do {
+    console.log(`Score is: ${score}`);
+    // score = score + 1
+    score++
+} while (score <= 10);
